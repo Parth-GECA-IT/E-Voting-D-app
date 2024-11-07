2887737be77b00d98825959aeb6f810601b46b64
Parth-GECA-IT <armarkarparth17@geca.ac.in>
2024-11-08 00:51:31 +0530

    Minor updations

--- Components/Global/Header/Header.jsx
+++ Components/Global/Header/Header.jsx
@@ -15,12 +15,12 @@ const Header = () => {
   };
 
   return (
-    <header class="header-section a2-bg-0 header-section--secondary header-menu w-100">
-      <div class="container d-center ">
-        <nav class="navbar a2-lg-bg py-5 p-lg-5 rounded-3 navbar-expand-lg w-100 justify-content-between ">
-          <div class="d-flex align-items-center">
+    <header className="header-section a2-bg-0 header-section--secondary header-menu w-100">
+      <div className="container d-center ">
+        <nav className="navbar a2-lg-bg py-5 p-lg-5 rounded-3 navbar-expand-lg w-100 justify-content-between ">
+          <div className="d-flex align-items-center">
             <button
-              class="navbar-toggler ms-4"
+              className="navbar-toggler ms-4"
               type="button"
               data-bs-toggle="collapse"
               aria-label="Navbar Toggler"
@@ -35,114 +35,114 @@ const Header = () => {
             </button>
             <a
               href="/"
-              class="navbar-brand m-0 p-0 d-flex align-items-center gap-5 gap-xl-5 me-2"
+              className="navbar-brand m-0 p-0 d-flex align-items-center gap-5 gap-xl-5 me-2"
             >
               <img
                 src="assets/images/LogoF.png"
-                class="logo small_logo d-sm-none"
+                className="logo small_logo d-sm-none"
                 alt="logo"
               />
               <img
                 src="assets/images/LogoF.png"
-                class="logo d-none d-lg-flex"
+                className="logo d-none d-lg-flex"
                 style = {{width:'4em'}}
                 alt="logo"
               />
-              <span className="d-none d-lg-flex" 
+              <span classNameName="d-none d-lg-flex" 
                 style={{ left: '5.1rem', position: 'absolute', fontSize: '38px', fontFamily: 'arial' }}
                 >otz</span>
             </a>
           </div>
-          <div class="nav_alt">
-            <div class="right-area position-relative ms-0 d-center gap-1 gap-xl-4 d-lg-none">
+          <div className="nav_alt">
+            <div className="right-area position-relative ms-0 d-center gap-1 gap-xl-4 d-lg-none">
               {address ? (
                 <>
-                  <div class="single-item">
+                  <div className="single-item">
                     <a
                       href="/voter"
-                      class="rotate_eff flex-nowrap py-1 px-2 px-xl-3 d-center gap-1 fw-bold nw1-color"
+                      className="rotate_eff flex-nowrap py-1 px-2 px-xl-3 d-center gap-1 fw-bold nw1-color"
                     >
                       {" "}
-                      Voter <i class="ti ti-arrow-right fs-six-up"></i>
+                      Voter <i className="ti ti-arrow-right fs-six-up"></i>
                     </a>
                   </div>
-                  <div class="single-item">
+                  <div className="single-item">
                     <a
                       href="/candidate"
-                      class="cmn-btn fw-bold py-2 px-2 px-sm-3 px-lg-4 align-items-center gap-1"
+                      className="cmn-btn fw-bold py-2 px-2 px-sm-3 px-lg-4 align-items-center gap-1"
                     >
                       {" "}
                       Candidate{" "}
-                      <i class="ti ti-arrow-right fw-semibold fs-six-up"></i>
+                      <i className="ti ti-arrow-right fw-semibold fs-six-up"></i>
                     </a>
                   </div>
                 </>
               ) : (
-                <div class="single-item">
+                <div className="single-item">
                   <a
                     onClick={() => clickConnect()}
-                    class="cmn-btn fw-bold py-2 px-2 px-sm-3 px-lg-4 align-items-center gap-1"
+                    className="cmn-btn fw-bold py-2 px-2 px-sm-3 px-lg-4 align-items-center gap-1"
                   >
                     {" "}
                     Connect Wallet{" "}
-                    <i class="ti ti-arrow-right fw-semibold fs-six-up"></i>
+                    <i className="ti ti-arrow-right fw-semibold fs-six-up"></i>
                   </a>
                 </div>
               )}
             </div>
           </div>
           <div
-            class="collapse navbar-collapse justify-content-center"
+            className="collapse navbar-collapse justify-content-center"
             id="navbar-content"
           >
-            <ul class="navbar-nav gap-2 gap-lg-3 gap-xxl-8  align-self-center mx-auto mt-4 mt-lg-0">
-              <li class="dropdown show-dropdown">
-                <a href="/" class=" dropdown-nav active">
+            <ul className="navbar-nav gap-2 gap-lg-3 gap-xxl-8  align-self-center mx-auto mt-4 mt-lg-0">
+              <li className="dropdown show-dropdown">
+                <a href="/" className=" dropdown-nav active">
                   home
                 </a>
               </li>
-              <li class="dropdown show-dropdown">
+              <li className="dropdown show-dropdown">
                 <button
                   type="button"
                   aria-label="Navbar Dropdown Button"
-                  class=" dropdown-nav"
+                  className=" dropdown-nav"
                 >
                   Candidate
                 </button>
-                <ul class="dropdown-menu">
+                <ul className="dropdown-menu">
                   <li>
-                    <a class="dropdown-item" href="/register-candidate">
+                    <a className="dropdown-item" href="/register-candidate">
                       All Candidate
                     </a>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="/approve-candidate">
+                    <a className="dropdown-item" href="/approve-candidate">
                       Approve Candidate
                     </a>
                   </li>
                 </ul>
               </li>
-              <li class="dropdown show-dropdown">
+              <li className="dropdown show-dropdown">
                 <button
                   type="button"
                   aria-label="Navbar Dropdown Button"
-                  class=" dropdown-nav"
+                  className=" dropdown-nav"
                 >
                   Voter
                 </button>
-                <ul class="dropdown-menu">
+                <ul className="dropdown-menu">
                   <li>
-                    <a class="dropdown-item" href="/register-voter">
+                    <a className="dropdown-item" href="/register-voter">
                       All Voters
                     </a>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="/approve-voter">
+                    <a className="dropdown-item" href="/approve-voter">
                       Approve Voters
                     </a>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="/all-voters-voted">
+                    <a className="dropdown-item" href="/all-voters-voted">
                       All Voted Voters
                     </a>
                   </li>
@@ -151,52 +151,52 @@ const Header = () => {
 
               {address == OWNER_ADDRESS?.toLowerCase() && (
                 <li>
-                  <a class="dropdown-item" href="/owner">
+                  <a className="dropdown-item" href="/owner">
                     Owner
                   </a>
                 </li>
               )}
-              <li class="dropdown show-dropdown">
+              <li className="dropdown show-dropdown">
                 <button
                   type="button"
                   aria-label="Navbar Dropdown Button"
-                  class=" dropdown-nav"
+                  className=" dropdown-nav"
                 >
                   {/* Resources */}
                 </button>
-                <ul class="dropdown-menu">
+                <ul className="dropdown-menu">
                   <li>
-                    <a class="dropdown-item" href="/about">
+                    <a className="dropdown-item" href="/about">
                       About Us
                     </a>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="/legal-docs">
+                    <a className="dropdown-item" href="/legal-docs">
                       legal docs
                     </a>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="/contact">
+                    <a className="dropdown-item" href="/contact">
                       Contact Us
                     </a>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="/roadmap">
+                    <a className="dropdown-item" href="/roadmap">
                       roadmap
                     </a>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="/support">
+                    <a className="dropdown-item" href="/support">
                       support
                     </a>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="/terms-conditions">
+                    <a className="dropdown-item" href="/terms-conditions">
                       terms conditions
                     </a>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="/privacy-policy">
+                    <a className="dropdown-item" href="/privacy-policy">
                       privacy-policy
                     </a>
                   </li>
@@ -204,38 +204,38 @@ const Header = () => {
               </li>
             </ul>
           </div>
-          <div class="right-area position-relative  ms-0 d-center gap-1 gap-xl-4 d-none d-lg-flex">
+          <div className="right-area position-relative  ms-0 d-center gap-1 gap-xl-4 d-none d-lg-flex">
             {address ? (
               <>
-                <div class="single-item">
+                <div className="single-item">
                   <a
                     href="/voter"
-                    class="rotate_eff flex-nowrap py-1 px-2 px-xl-3 d-center gap-1 fw-bold nw1-color"
+                    className="rotate_eff flex-nowrap py-1 px-2 px-xl-3 d-center gap-1 fw-bold nw1-color"
                   >
                     {" "}
-                    Voter <i class="ti ti-arrow-right fs-six-up"></i>
+                    Voter <i className="ti ti-arrow-right fs-six-up"></i>
                   </a>
                 </div>
-                <div class="single-item">
+                <div className="single-item">
                   <a
                     href="/candidate"
-                    class="cmn-btn fw-bold py-2 px-2 px-sm-3 px-lg-4 align-items-center gap-1"
+                    className="cmn-btn fw-bold py-2 px-2 px-sm-3 px-lg-4 align-items-center gap-1"
                   >
                     {" "}
                     Candidate{" "}
-                    <i class="ti ti-arrow-right fw-semibold fs-six-up"></i>
+                    <i className="ti ti-arrow-right fw-semibold fs-six-up"></i>
                   </a>
                 </div>
               </>
             ) : (
-              <div class="single-item">
+              <div className="single-item">
                 <a
                   onClick={() => clickConnect()}
-                  class="cmn-btn fw-bold py-2 px-2 px-sm-3 px-lg-4 align-items-center gap-1"
+                  className="cmn-btn fw-bold py-2 px-2 px-sm-3 px-lg-4 align-items-center gap-1"
                 >
                   {" "}
                   Connet Wallet{" "}
-                  <i class="ti ti-arrow-right fw-semibold fs-six-up"></i>
+                  <i className="ti ti-arrow-right fw-semibold fs-six-up"></i>
                 </a>
               </div>
             )}
