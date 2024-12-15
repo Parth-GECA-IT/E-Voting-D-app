ed038262663ccabe7743bd133649b63cbe642a89
Parth-GECA-IT <armarkarparth17@geca.ac.in>
2024-12-15 19:18:32 +0530

    Owner Problem solved

--- context/constants.js
+++ context/constants.js
@@ -5,7 +5,7 @@ import axios from "axios";
 //IMPORT CONTRACT ABI
 import votingOrganization from "./VotingOrganization.json";
 
-export const OWNER_ADDRESS = "0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65";
+export const OWNER_ADDRESS = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 
 export const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 export const CONTRACT_ABI = votingOrganization.abi;
